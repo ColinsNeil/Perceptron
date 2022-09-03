e2383fd162c522fae1181692f1f2d1e1f1e4026f
Colins Neil <colinsneilnimalaraj@gmail.com>
2022-09-03 17:28:30 +0200

    Refonte du code

--- perceptron_simple.py
+++ perceptron_simple.py
@@ -1,64 +1,18 @@
-data_set = [
-    (1, 1, 1),
-    (1, 1, 2),
-    (1, 2, 1),
-    (1, 3, 3),
-    (1, 3, 4),
-    (1, 4, 3),
-    (1, 4, 4)
-    ]
+def Estimation(entrees, poids):
+    """
+    Desc : Fait le produit scalaire des entrées avec les poids pour faire une prédiction
 
-data_set_res = [
-    1,
-    1,
-    1,
-    0,
-    0,
-    0,
-    0
-]
+    Paramètres : 
+            entrees : Nombre totales des entrées
+            poids : poids de chaque entrée
 
-def estimation(poids, caracteres):
-    nb_poids = len(poids)
+    Retourne :
+            res : Prédiction par rapport aux entrées et aux poids
+    """
+    nb_entrees = len(entrees)
     res = 0
 
-    for i in range(nb_poids):
-        res += poids[i] * caracteres[i]
-
+    for i in range(nb_entrees):
+        res += entrees[i] * poids[i]
+    
     return res
-
-def miseAJourPoids(poids, caractere, estim, res):
-    vitesse_apprentissage = 0.1
-
-    return poids + vitesse_apprentissage * ( res - estim ) * caractere
-
-def apprentissage(exemples):
-    nb_poids = len(exemples[0])
-    nb_exemples = len(exemples)
-
-    poids = []
-    estim = []
-
-    for i in range(nb_poids):
-        poids.append(0)
-
-    for i in range(nb_exemples):
-        exemple = exemples[i]
-
-        estim.append(estimation(poids, exemple))
-
-        for j in range(nb_poids):
-            poids[j] = miseAJourPoids(poids[j], exemple[j], estim[i], data_set_res[i])
-
-    print(poids)
-
-    """
-
-    x1 = 4
-    x2 = 4
-    print(1 * poids[0] + x1 * poids[1] + x2 * poids[2])
-
-    """
-
-
-apprentissage(data_set)
